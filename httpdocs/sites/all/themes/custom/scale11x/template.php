6fa589af120027ed037a0a9339f84dbd09d57a33
Ron Golan <ron@rgon.us>
2012-09-22 13:48:45 -0700

    Replace jQuery library to scale11x theme and correct template.php and .info.

--- httpdocs/sites/all/themes/custom/scale11x/template.php
+++ httpdocs/sites/all/themes/custom/scale11x/template.php
@@ -1,5 +1,5 @@
 <?php
 function scale11x_js_alter(&$javascript) {
-  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'scale11x').'/js/jquery.js';
+  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'scale11x').'/js/jquery.min.js';
   $javascript['misc/jquery.js']['version'] = '1.7.2';
 }
