7589a5288e418513b7bd7913f19c0d383517660a
Ron Golan <ron@rgon.us>
2014-06-29 14:37:27 -0700

    Add scale13x Bootstrap subtheme.

---
+++ httpdocs/sites/all/themes/custom/scale13x/template.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file template.php
+ */
+
+/**
+ * Preprocess variables for page.tpl.php
+ *
+ * @see page.tpl.php
+ */
+function scale13x_preprocess_page(&$variables) {
+  // Add information about the number of sidebars.
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 3;
+  }
+  elseif (!empty($variables['page']['sidebar_first'])) {
+    $variables['columns'] = 2;
+  }
+  elseif (!empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 2;
+  }
+  else {
+    $variables['columns'] = 1;
+  }
+
+  // Primary nav
+  $variables['primary_nav'] = FALSE;
+  if ($variables['main_menu']) {
+    // Build links
+    //$variables['primary_nav'] = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
+    $variables['primary_nav'] = menu_tree('menu-12x-main');
+    // Provide default theme wrapper function
+    $variables['primary_nav']['#theme_wrappers'] = array('menu_tree__primary');
+  }
+
+  // Secondary nav
+  $variables['secondary_nav'] = FALSE;
+  if ($variables['secondary_menu']) {
+    // Build links
+    $variables['secondary_nav'] = menu_tree(variable_get('menu_secondary_links_source', 'user-menu'));
+    // Provide default theme wrapper function
+    $variables['secondary_nav']['#theme_wrappers'] = array('menu_tree__secondary');
+  }
+
+}
+
