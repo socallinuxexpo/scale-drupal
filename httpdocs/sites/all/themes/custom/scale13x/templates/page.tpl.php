1800153d3f7f46b2c3e45c17077842ba4e3672a7
Ron Golan <ron@rgon.us>
2015-01-16 19:23:12 -0800

    Remove breadcrumbs

--- httpdocs/sites/all/themes/custom/scale13x/templates/page.tpl.php
+++ httpdocs/sites/all/themes/custom/scale13x/templates/page.tpl.php
@@ -71,7 +71,7 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <?php //if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
