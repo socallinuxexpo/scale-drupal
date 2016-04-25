27e4ef49a2a29d521fcd3d68a85ff1daa5868b0a
Ron Golan <ron@rgon.us>
2016-04-24 17:22:56 -0700

    Add scale15x theme.

---
+++ httpdocs/sites/all/themes/custom/scale15x/template.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Theme PHP code.
+ */
+
+/**
+ * Preprocess variables for page.tpl.php.
+ *
+ * @see page.tpl.php
+ */
+function scale15x_preprocess_page(&$variables) {
+  // Add information about the number of sidebars.
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 3;
+  }
+  elseif (!empty($variables['page']['sidebar_first'])) {
+    $variables['columns'] = 2;
+  }
+  elseif (!empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 2;
+  }
+  else {
+    $variables['columns'] = 1;
+  }
+
+  // Primary nav.
+  $variables['primary_nav'] = FALSE;
+  if ($variables['main_menu']) {
+    // Build links
+    // $variables['primary_nav'] = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
+    $variables['primary_nav'] = menu_tree('menu-15x-main');
+    // Provide default theme wrapper function.
+    $variables['primary_nav']['#theme_wrappers'] = array('menu_tree__primary');
+  }
+
+  // Secondary nav.
+  $variables['secondary_nav'] = FALSE;
+  if ($variables['secondary_menu']) {
+    // Build links.
+    $variables['secondary_nav'] = menu_tree(variable_get('menu_secondary_links_source', 'user-menu'));
+    // Provide default theme wrapper function.
+    $variables['secondary_nav']['#theme_wrappers'] = array('menu_tree__secondary');
+  }
+}
+
+function scale15x_preprocess_block(&$variables) {
+  // Show Guidebook link block only on mobile.
+  $block_id = $variables['block']->module . '-' . $variables['block']->delta;
+  if ($block_id == 'block-11') {
+    $variables['classes_array'][] = 'visible-phone';
+  }
+}
