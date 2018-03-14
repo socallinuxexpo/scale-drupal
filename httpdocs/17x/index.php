fb167a31962269ce36e20453d7778a46844e66be
Ron Golan <ron@rgon.us>
2018-03-13 21:16:53 -0700

    Add 17x save the date page

---
+++ httpdocs/17x/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <title>Save the Date</title>
+  <link href="style.css">
+<style>
+.outer-frame {
+  position: absolute;
+  top: 50%;
+  height: 0;
+  width: 100%;
+  overflow: visible;
+}
+.inner-frame {
+  position: relative;
+  margin: 0 auto;
+  width: 90%;
+  max-width: 415px;
+  top: -310px;
+  text-align: center;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+}
+.inner-frame img {
+  max-width: 100%;
+}
+.inner-frame p {
+  text-align: left;
+  margin: 0 5px;
+}
+.register {
+  background-color: red;
+  color: white;
+  padding: 10px 15px;
+  border-radius: 20px;
+  text-decoration: none;
+  cursor: pointer;
+  position: relative;
+  top: 20px;
+}
+</style>
+</head>
+<body>
+  <div class="outer-frame">
+    <div class="inner-frame">
+      <img src="img/scale17x_logo_outline2.svg">
+      <p>March 7-10, 2019</p>
+      <p>Pasadena Convention Center</p>
+      <a class="register" href="https://register.socallinuxexpo.org/reg6/">Register</a>
+    </div>
+  </div>
+</body>
+</html>
