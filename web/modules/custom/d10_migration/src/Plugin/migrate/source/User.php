779127baca82929d14b672498040de2f81c14074
Phillip Smith <drupalphil@gmail.com>
2023-10-10 21:07:54 -0400

    Adds User migration

---
+++ web/modules/custom/d10_migration/src/Plugin/migrate/source/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\d10_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+
+/**
+ * @MigrateSource(
+ *  id = "user_source",
+ * )
+ */
+class User extends SourcePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initializeIterator() {
+    $client = \Drupal::service('d10_migration.client');
+    $response = $client->get('/migrate/user/json/test');
+
+    return new \ArrayIterator($response);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'uid' => [
+        'type' => 'string',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    return "User";
+  }
+
+}
