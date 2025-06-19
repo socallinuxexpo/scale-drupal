1ba00baf64fba19d809dbf35b200e1a9829991d8
Phillip Smith <drupalphil@gmail.com>
2025-06-19 16:46:22 -0400

    Only displays grouped select lists on edit form

--- web/modules/custom/utility/modules/allowed_taxonomy/src/Plugin/Field/FieldWidget/GroupedSelectWidget.php
+++ web/modules/custom/utility/modules/allowed_taxonomy/src/Plugin/Field/FieldWidget/GroupedSelectWidget.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\allowed_taxonomy\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsSelectWidget;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'grouped_select_widget' widget.
@@ -20,103 +23,119 @@ use Drupal\Core\Form\FormStateInterface;
 class GroupedSelectWidget extends OptionsSelectWidget {
 
   /**
+   * The entity type manager.
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The route match service.
+   */
+  protected RouteMatchInterface $routeMatch;
+
+  /**
    * {@inheritdoc}
    */
-  protected function supportsGroups() {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    $instance->routeMatch = $container->get('current_route_match');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function supportsGroups(): bool {
     return TRUE;
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getOptions(FieldableEntityInterface $entity) {
-    if (!isset($this->options)) {
-      $handler_settings = $this->fieldDefinition->getSetting('handler_settings');
-      $bundles = isset($handler_settings['target_bundles']) ? $handler_settings['target_bundles'] : [];
+  protected function getOptions(FieldableEntityInterface $entity): array {
+    if (isset($this->options)) {
+      return $this->options;
+    }
 
-      foreach ($bundles as $bundle) {
-        if ($bundle === 'event') {
-          $this->getGroupedEntityOptions($bundle);
-        } else {
-          $this->getGroupedTaxonomyOptions($bundle);
-        }
-      }
+    $this->options = [];
+    $bundles = $this->getTargetBundles();
+
+    foreach ($bundles as $bundle) {
+      $options = $bundle === 'event'
+        ? $this->getEventOptions()
+        : $this->getTaxonomyOptions($bundle);
+
+      $this->options = array_merge($this->options, $options);
     }
 
     return $this->options;
   }
 
-  protected function getGroupedEntityOptions($bundle) {
-    $active_options = [];
-    $inactive_options = [];
-
-    $items = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->loadByProperties(['type' => $bundle]);
-
-    foreach ($items as $item) {
-      if(!$item->hasField('field_event_date')) {
-        continue;
-      }
-
-      // If field_event_date start date is in the past, return active (true)
-      $active_value = $item->get('field_event_date')->value > date('Y-m-d');
-      $option_key = $item->id();
-      $option_label = $item->label();
-
-      if ($active_value) {
-        $active_options[$option_key] = $option_label;
-      } else {
-        $inactive_options[$option_key] = $option_label;
-      }
-    }
-
-    $this->options = [];
-    if (!empty($active_options)) {
-      $this->options['Active'] = $active_options;
-    }
-    if (!empty($inactive_options)) {
-      $this->options['Inactive'] = $inactive_options;
-    }
+  /**
+   * Gets target bundles from field settings.
+   */
+  private function getTargetBundles(): array {
+    $handler_settings = $this->fieldDefinition->getSetting('handler_settings');
+    return $handler_settings['target_bundles'] ?? [];
   }
 
-  protected function getGroupedTaxonomyOptions($bundle) {
-    $active_options = [];
-    $inactive_options = [];
+  /**
+   * Gets grouped options for event entities.
+   */
+  private function getEventOptions(): array {
+    $entities = $this->entityTypeManager
+      ->getStorage('node')
+      ->loadByProperties(['type' => 'event']);
 
-    $items = \Drupal::entityTypeManager()
+    return $this->buildGroupedOptions($entities, function ($entity) {
+      return $entity->hasField('field_event_date')
+        && $entity->get('field_event_date')->value > date('Y-m-d');
+    });
+  }
+
+  /**
+   * Gets grouped options for taxonomy terms.
+   */
+  private function getTaxonomyOptions(string $bundle): array {
+    $entities = $this->entityTypeManager
       ->getStorage('taxonomy_term')
       ->loadByProperties(['vid' => $bundle]);
 
-    foreach ($items as $item) {
-      if(!$item->hasField('field_active')) {
-        continue;
-      }
-      $active_value = $item->get('field_active')->value;
-      $option_key = $item->id();
-      $option_label = $item->getName();
+    return $this->buildGroupedOptions($entities, function ($entity) {
+      return $entity->hasField('field_active')
+        && $entity->get('field_active')->value;
+    });
+  }
 
-      if ($active_value) {
-        $active_options[$option_key] = $option_label;
-      } else {
-        $inactive_options[$option_key] = $option_label;
+  /**
+   * Builds grouped options array from entities.
+   */
+  private function buildGroupedOptions(array $entities, callable $isActiveCallback): array {
+    $active = [];
+    $inactive = [];
+    $isEditForm = $this->routeMatch->getRouteName() === 'entity.node.edit_form';
+
+    foreach ($entities as $entity) {
+      $key = $entity->id();
+      $label = method_exists($entity, 'getName') ? $entity->getName() : $entity->label();
+
+      if ($isActiveCallback($entity)) {
+        $active[$key] = $label;
+      } elseif ($isEditForm) {
+        $inactive[$key] = $label;
       }
     }
 
-    $this->options = [];
-    if (!empty($active_options)) {
-      $this->options['Active'] = $active_options;
-    }
-    if (!empty($inactive_options)) {
-      $this->options['Inactive'] = $inactive_options;
-    }
+    return array_filter([
+      'Active' => $active ?: null,
+      'Inactive' => $inactive ?: null,
+    ]);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
-    return $values[0][0]["target_id"];
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state): string {
+    return $values[0][0]['target_id'] ?? '';
   }
 }
-
